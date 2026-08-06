d61ffc0262538d4921edfd4c80130495d32ed806
Youssefwafa7 <youssef.mwafa@gmail.com>
2026-08-06 23:51:57 +0300

    refactor: use lucide icons instead of pngs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Image } from 'react-native';
+import { Home, ShoppingBag, ShoppingCart, Heart, User } from 'lucide-react-native';
 
 import { HapticTab } from '@shared/components/haptic-tab';
 import { Colors, FontFamily } from '@shared/constants/theme';
@@ -23,31 +23,18 @@ export default function TabLayout() {
           fontSize: 10,
         },
         tabBarIcon: ({ color, size }) => {
-          let iconSource;
-
           if (route.name === 'index') {
-            iconSource = require('../../assets/icons/home_tab.png');
+            return <Home color={color} size={size} />;
           } else if (route.name === 'shop') {
-            iconSource = require('../../assets/icons/shop_tab.png');
+            return <ShoppingBag color={color} size={size} />;
           } else if (route.name === 'cart') {
-            iconSource = require('../../assets/icons/cart_tab.png');
+            return <ShoppingCart color={color} size={size} />;
           } else if (route.name === 'wishlist') {
-            iconSource = require('../../assets/icons/wishlist_tab.png');
+            return <Heart color={color} size={size} />;
           } else if (route.name === 'profile') {
-            iconSource = require('../../assets/icons/profile_tab.png');
+            return <User color={color} size={size} />;
           }
-
-          return (
-            <Image
-              source={iconSource}
-              style={{
-                width: size,
-                height: size,
-                tintColor: color,
-              }}
-              resizeMode="contain"
-            />
-          );
+          return null;
         },
       })}>
       <Tabs.Screen
